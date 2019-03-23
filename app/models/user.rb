845438afb3f05b2143cd8e84dc14cae0d54df10a
DonEvans <don9450-register@yahoo.com>
2019-03-22 18:57:11 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :microposts, dependent: :destroy
 	before_save {email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }
@@ -14,5 +15,12 @@ class User < ApplicationRecord
 															BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
+
+	#Defines a proto-feed.
+	#See "Following users" for the full implementation
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
+
 end
 
